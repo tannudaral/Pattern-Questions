c23b0503b8d88dc593696adc4bbd48e4f8f9d133
Tannu Daral <70472388+tannudaral@users.noreply.github.com>
2021-04-08 09:30:33 +0530

    Create Rectangle.cpp
    Prints a rectangular pattern.

---
+++ Patterns-code/Rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int i, j, row, col;
+   cout<<"Enter the number of rows and columns: ";
+   cin>>row>>col;
+   for(i=0; i<row; i++)
+   {
+      for(j=0; j<col; j++)
+        cout<<"* ";
+    cout<<endl;  
+   }
+   return 0;
+}
